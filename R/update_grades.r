e5df95fee03dd9c5b9d393accbfde97b608d7c98
shlrley <shirley_zh@icloud.com>
2023-01-20 19:08:11 -0800

    Add docstrings for update_assignment_grade() method

--- R/update_grades.r
+++ R/update_grades.r
@@ -28,3 +28,20 @@ load_course <- function(input_file) {
 save_course_csv <- function(updated_course_df, output_file) {
     
 }
+
+#' Saves the updated course information to a .csv file 
+#'
+#' Takes the data.frame of the course with updated grades, the path to the output 
+#' file, and saves the data.frame as a .csv 
+#'
+#' @param input_file path to the 'course.csv' file 
+#' @param component name of the component (or assignment) of the course to be updated
+#' @param grade numeric grade of the component (or assignment) to be inserted into the data.frame
+#'
+#' @return None
+#' @export
+#'
+#' @examples update_assignment_grade('DSCI100.csv', 'Assignment 1', 95.0)
+update_assignment_grade <- function(input_file, component, grade) {
+    
+}
