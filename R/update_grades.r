f952398bd6b7d6be03229ed7f91e485c761d6304
shlrley <shirley_zh@icloud.com>
2023-01-20 19:06:33 -0800

    Added docstrings for load_course() method

---
+++ R/update_grades.r
@@ -0,0 +1,14 @@
+#' Loads the 'course.csv' file 
+#'
+#' Takes the path to the course's .csv file as an argument ('course.csv'), reads the file,
+#' and saves the file as a dataframe to be returned 
+#'
+#' @param input_file path to the 'course.csv' file 
+#'
+#' @return data.frame which holds the information from 'course.csv' 
+#' @export
+#'
+#' @examples load_course('DSCI100.csv')
+load_course <- function(input_file) {
+    
+}
