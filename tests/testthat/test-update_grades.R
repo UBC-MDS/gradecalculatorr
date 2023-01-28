1093e7d4180bd94952f68eb0021df4916d9d7d8e
shlrley <shirley_zh@icloud.com>
2023-01-27 19:11:10 -0800

    Added second unit test for update_grades

--- tests/testthat/test-update_grades.R
+++ tests/testthat/test-update_grades.R
@@ -14,3 +14,30 @@ test_that('update_component_grade() updates the grade of a component in the .csv
     
 }) 
 
+test_that('update_component_grade() only updates the one row that was specified to be updated', {
+
+    # set values
+    input_file_path <- 'dummycourse.csv'
+    component <- "Assignment 3"
+    expected <- 97
+
+    # get original grades list 
+    df <- read_csv(file=input_file_path)
+    df_og_list <- df$`Grades (%)`
+
+    # update csv and get new list
+    update_component_grade(input_file_path, component, expected)
+    df2 <- read_csv(file=input_file_path)
+    df2_list <- df2$`Grades (%)`
+
+    # get index of component 
+    comp_index <- which(df$Components == component)
+
+    # update original grades list with expected grade 
+    df_og_list[comp_index] <- expected 
+
+    # compare the two lists 
+    expect_equal(df_og_list, df2_list)    
+    
+})
+
