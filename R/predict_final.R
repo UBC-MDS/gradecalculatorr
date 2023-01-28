f431e9b0785a1cbdf715fe8329d4ee4ed0dc4215
Yukun Zhang <ykzhang1211@g.ucla.edu>
2023-01-28 14:50:04 -0800

    complete predict_final() function

--- R/predict_final.R
+++ R/predict_final.R
@@ -1,3 +1,8 @@
+library(readr)
+library(tidyr)
+library(devtools)
+library(testthat)
+
 #' Calculate what grade the student needs to have for the final assignment in order to reach the goal for overall grade
 #' and return the scores needed for the final assignment.
 #'
@@ -7,7 +12,36 @@
 #' @return final_score_needed which is the scores needed for the final assignment. Otherwise return warning message.
 #'
 #' @examples
-#' predict_final('/DSCI100.csv', 90.0)
+#' predict_final('DSCI100.csv', 90.0)
 predict_final <- function(input_file, goal) {
+
+  # read the input csv file
+  course_info <- read_csv(file=input_file)
+
+  # Check if all assignment weight sum up to 100%
+  if (sum(course_info$`Weights (%)`) != 100) {
+    error_msg <- "Your total assignment weights are not summing up to 100%! Please update all your assignments."
+    final_score_needed <- error_msg
+    return (final_score_needed)
+  }
+
+  # Check if final score (in the last row) is NA
+  if (! is.na(course_info$`Grades (%)`[length(course_info$`Grades (%)`)])) {
+    error_msg <- "Your final assignment score is already updated. Please directly use calculate_grade()."
+    final_score_needed <- error_msg
+    return (final_score_needed)
+  }
+
+  # Check if there if more than one NA value in the 'Grades (%)' column
+  if (sum(is.na(course_info$`Grades (%)`)) > 1) {
+    error_msg <- "Please update all your assignment scores except the final."
+    final_score_needed <- error_msg
+    return (final_score_needed)
+  }
+
+  # Calculate final_score_needed with given goal when only score of final is NA
+  total_scores_except_final <- sum(course_info$`Grades (%)` * course_info$`Weights (%)`, na.rm=TRUE)
+  final_score_needed <- round((goal*100 - total_scores_except_final)/course_info$`Weights (%)`[length(course_info$`Weights (%)`)], 1)
+  return (final_score_needed)
 
 }
